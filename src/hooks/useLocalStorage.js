0e69fa3ed92b94f9d5b00b6fce5dc7158808ddf7
Raj <rs91963@gmail.com>
2023-05-14 22:32:32 +0530

    Configure React app for deployment to GitHub Pages

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,7 +1,7 @@
 import {useEffect, useState} from 'react'
 
 
-const PREFIX = 'codepen-clone-'
+const PREFIX = 'codepen-clone'
 
 export default function useLocalStorage(key,initialValue) {
     const prefixedKey = PREFIX + key
