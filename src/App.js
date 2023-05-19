eb6c2bf2d402a7137e2552f7a44c64defb58ac52
Raj <rs91963@gmail.com>
2023-05-19 23:55:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,56 +1,60 @@
-import { useState , useEffect } from "react";
+import { useState, useEffect } from "react";
 import React from "react";
 import Editor from "./Editor";
 import useLocalStorage from "./hooks/useLocalStorage";
 
 function App() {
-  const [html, setHtml] = useLocalStorage('html', '')
-  const [css, setCss] = useLocalStorage('css', '')
-  const [js, setJs] = useLocalStorage('js', '')
-  const [srcDoc, setsrcDoc] = useState('')
+  const [html, setHtml] = useLocalStorage("html", "");
+  const [css, setCss] = useLocalStorage("css", "");
+  const [js, setJs] = useLocalStorage("js", "");
+  const [srcDoc, setsrcDoc] = useState("");
 
   useEffect(() => {
-    const timeout = setTimeout(() => { setsrcDoc(`
+    const timeout = setTimeout(() => {
+      setsrcDoc(`
                                      <html>
                                      <body>${html}</body>
                                      <style>${css}</style>
                                      <script>${js}</script>
-                                     </html>`)},250)
-                                      return () => clearTimeout(timeout)
-                                     }, [html,css,js])
+                                     </html>`);
+    }, 250);
+    return () => clearTimeout(timeout);
+  }, [html, css, js]);
 
   return (
     <>
       <div className="pane top-pane">
-        <Editor language="xml" 
-        displayName="HTML" 
-        value={html} 
-        onChange={setHtml} 
+        <Editor
+          language="xml"
+          displayName="HTML"
+          value={html}
+          onChange={setHtml}
         />
-        <Editor language="css"
-        displayName="CSS"
-        value={css}
-        onChange={setCss}
+        <Editor
+          language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setCss}
         />
-        <Editor language="javascript"
-        displayName="JS"
-        value={js}
-        onChange={setJs} 
+        <Editor
+          language="javascript"
+          displayName="JS"
+          value={js}
+          onChange={setJs}
         />
       </div>
       <div className="pane">
         <iframe
-         srcDoc = {srcDoc}
+          srcDoc={srcDoc}
           title="output"
           sandbox="allow-scripts"
           frameBorder="0"
           width="100%"
           height="100%"
-
         />
       </div>
     </>
-  )
+  );
 }
 
 export default App;
